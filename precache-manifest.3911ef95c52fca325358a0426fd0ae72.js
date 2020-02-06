f1c4f7ebd36eadbe6e351a2b808c0e5ef413a9d4
beomhee-lee <beomhee.lee@linecorp.com>
2020-02-06 11:05:36 +0900

    v0.5

---
+++ precache-manifest.3911ef95c52fca325358a0426fd0ae72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682393ee68b95b351770",
+    "url": "/billiard-score-board-online/css/app.f1924cf0.css"
+  },
+  {
+    "revision": "d1a35344bac0c7636879",
+    "url": "/billiard-score-board-online/css/chunk-vendors.0c8d826c.css"
+  },
+  {
+    "revision": "07262f6d130b98112eb65760e359d78f",
+    "url": "/billiard-score-board-online/index.html"
+  },
+  {
+    "revision": "682393ee68b95b351770",
+    "url": "/billiard-score-board-online/js/app.dbd21a87.js"
+  },
+  {
+    "revision": "d1a35344bac0c7636879",
+    "url": "/billiard-score-board-online/js/chunk-vendors.f918e570.js"
+  },
+  {
+    "revision": "b5b14f3797b5158fa539bee0ff305a25",
+    "url": "/billiard-score-board-online/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/billiard-score-board-online/robots.txt"
+  },
+  {
+    "revision": "b600b34ad3a4743a7276d769cb027fb1",
+    "url": "/billiard-score-board-online/service-worker.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/billiard-score-board-online/precache-manifest.c23ac9b7afda526f167a60548e78b77f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/billiard-score-board-online/precache-manifest.3911ef95c52fca325358a0426fd0ae72.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
   console.log(`Workbox is loaded`);
